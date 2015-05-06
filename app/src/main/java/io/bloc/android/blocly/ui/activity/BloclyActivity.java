2e99539ac42474817e75f4d2c11de3028f64085e
Ben <bwong199@my.bcit.ca>
2015-05-06 15:23:54 -0700

    Assignment 43 completed

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,8 +1,8 @@
 package io.bloc.android.blocly.ui.activity;
 
-import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,6 +10,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 
 import io.bloc.android.blocly.R;
@@ -18,16 +19,19 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 /**
  * Created by benwong on 2015-05-04.
  */
-public class BloclyActivity extends ActionBar {
+public class BloclyActivity extends ActionBarActivity {
 
     private ItemAdapter itemAdapter;
     private ActionBarDrawerToggle drawerToggle;
     private DrawerLayout drawerLayout;
-
+    static final int DRAWER_DELAY = 200;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.tb_activity_blocly);
         setSupportActionBar(toolbar);
@@ -45,6 +49,19 @@ public class BloclyActivity extends ActionBar {
 
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, 0, 0);
         drawerLayout.setDrawerListener(drawerToggle);
+
+        new Handler().postDelayed(openDrawerRunnable(), DRAWER_DELAY);
+
+    }
+
+    private Runnable openDrawerRunnable() {
+        return new Runnable() {
+
+            @Override
+            public void run() {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        };
     }
 
     @Override
@@ -69,6 +86,8 @@ public class BloclyActivity extends ActionBar {
         return super.onOptionsItemSelected(item);
 
     }
+
+
 
 
 
